6acecc55fd11d4ee9ef9c4248bbf9b2fbbc14dc3
ikolokotronis <ioanniskolokotronis1@gmail.com>
2022-01-26 12:50:32 +0100

    Add game over to tictactoe

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,13 +1,31 @@
 
 class TicTacToe:
     def __init__(self):
-        self.board = [' ' for x in range(10)]
+        self.board = [' ' for i in range(10)]
         self.game_over = False
 
     def game_loop(self):
         while self.game_over == False:
             self.print_board()
             self.user_input()
+            if self.isWinner("X"):
+                self.print_board()
+                print('Player X won!')
+                self.game_over = True
+            elif self.isWinner("O"):
+                self.print_board()
+                print('Player O won!')
+                self.game_over = True
+
+    def isWinner(self, player):
+        return (self.board[7] == player and self.board[8] == player and self.board[9] == player) or (
+                self.board[4] == player and self.board[5] == player and self.board[6] == player) or (
+                self.board[1] == player and self.board[2] == player and self.board[3] == player) or (
+                self.board[1] == player and self.board[4] == player and self.board[7] == player) or (
+                self.board[2] == player and self.board[5] == player and self.board[8] == player) or (
+                self.board[3] == player and self.board[6] == player and self.board[9] == player) or (
+                self.board[1] == player and self.board[5] == player and self.board[9] == player) or (
+                self.board[3] == player and self.board[5] == player and self.board[7] == player)
 
     def print_board(self):
         print(' ' + self.board[1] + ' | ' + self.board[2] + ' | ' + self.board[3])
@@ -26,10 +44,14 @@ class TicTacToe:
         
         get_letter = user_input_split[0]
         get_position = int(user_input_split[1])
-        self.insert_letter(get_letter, get_position)
+        if self.check_if_space_is_free(get_position):
+            self.insert_letter(get_letter, get_position)
+        else:
+            print('')
+            print("Position is taken!")
+            print('')
 
 
 ttt = TicTacToe()
 ttt.game_loop()
-
 
